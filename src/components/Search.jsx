fa273bbdbc5ae1a75efc425032955abffa0f5547
SwetaSharma7 <new.swetasharma.naju99@gmail/com>
2024-02-26 10:14:33 +0530

    css to search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDarkMode } from "./Darkmode";
+
 
 function Search({ handleSearch }) {
+  const { isDarkMode } = useDarkMode();
   return (
-    <div className="search">
+    <div className={isDarkMode ? "search search-DM" : "search"}>
       <FontAwesomeIcon icon={faSearch} className="search-icon" />
-      <input input="text" placeholder="Search for a Country" className="search-input" onKeyUp={handleSearch}></input>
+      <input input="text" placeholder="Search for a Country" className={isDarkMode ? "search-input search-DM" : "search-input"} onKeyUp={handleSearch}></input>
     </div>
   );
 }
